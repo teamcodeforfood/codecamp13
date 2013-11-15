fe7046426f34ead75ef1e4d88a5e9aad7e7ec4c8
Joshua Beitler <joshbeitler@gmail.com>
2013-11-15 08:29:20 -0700

    Cleanup

--- main.py
+++ main.py
@@ -3,17 +3,8 @@ from SpaceshipAdventure import SpaceshipAdventure
 
 def main():
     pygame.font.init()
-<<<<<<< HEAD
-<<<<<<< HEAD
-    c = SpaceshipAdventure(400, 600, 30)
-=======
-    pygame.mixer.init()
-    c = SpaceshipAdventure(1920, 1080, 30)
->>>>>>> f90f968527bfffd5765f2939658ade6802db1ee9
-=======
     pygame.mixer.init()
     c = SpaceshipAdventure(1920, 1080, 120)
->>>>>>> a45098ffef0858c57a7db865293725b9313c8b7e
     c.main_loop()
     return
     
