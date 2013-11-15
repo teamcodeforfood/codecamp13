7e13d7e39dc98e553d2010171cea7ebb4b34d21a
Joshua Beitler <joshbeitler@gmail.com>
2013-11-15 13:41:35 -0700

    Added plane class

---
+++ enemies/plane.py
@@ -0,0 +1,26 @@
+import pygame
+from baddie import Baddie
+import random
+
+class Plane(Baddie):
+    
+    def __init__(self, x, y):
+        self.color = (55, 55, 55)
+        self.width  = 20
+        self.height = 10
+        self.x      = 0
+        self.y      = random.randint(0, 720)
+        self.new_x  = y
+        self.new_y  = x
+        self.speed  = 1
+        self.alive  = True
+        pass
+
+    def tick(self, back_wall, upper_wall, lower_wall):
+        self.new_x = self.x + self.speed
+        if self.new_x > 720:
+            self.setAlive(False)
+        else:
+            self.x = self.new_x
+        self.y = self.new_y
+        return self.alive
