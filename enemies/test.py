3980a9ac7ed76761ae2bfd687e93235cd90d5333
Joshua Beitler <joshbeitler@gmail.com>
2013-11-15 13:17:21 -0700

    oops

---
+++ enemies/test.py
@@ -0,0 +1,17 @@
+import pygame
+from baddie import Baddie
+import random
+
+class TestBaddie(Baddie):
+    
+    def __init__(self, width, height, x, y, color):
+        self.color = (55, 55, 55)
+        self.width  = width
+        self.height = height
+        self.x      = random.randint(0,1280)
+        self.y      = 0
+        self.new_x  = y
+        self.new_y  = x
+        self.speed  = 1
+        self.alive  = True
+        pass
