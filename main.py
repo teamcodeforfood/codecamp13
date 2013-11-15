b02ac01b7b85ec25fbe7a4078fd850b7fc9c106e
Kyler Tolleson <kylertoll@yahoo.com>
2013-11-15 08:23:27 -0700

    starting to change orientation

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ from SpaceshipAdventure import SpaceshipAdventure
 
 def main():
     pygame.font.init()
-    c = SpaceshipAdventure(600, 400, 30)
+    c = SpaceshipAdventure(400, 600, 30)
     c.main_loop()
     return
     
 if __name__ == "__main__":
     main()
-
