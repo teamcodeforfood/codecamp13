138ee498730c5d4be9e948333653a5d4235fe189
Kyler Tolleson <kylertoll@yahoo.com>
2013-11-15 08:28:10 -0700

    Revert "Merge branch 'master' of https://github.com/teamcodeforfood/codecamp13"
    Conflicts:
    	main.py

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from SpaceshipAdventure import SpaceshipAdventure
 
 def main():
     pygame.font.init()
+<<<<<<< HEAD
     c = SpaceshipAdventure(400, 600, 30)
+=======
+    pygame.mixer.init()
+    c = SpaceshipAdventure(1920, 1080, 30)
+>>>>>>> f90f968527bfffd5765f2939658ade6802db1ee9
     c.main_loop()
     return
     
