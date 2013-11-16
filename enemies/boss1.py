b3dce2b5e41cef7490fea297cfa9778f7b7f3815
Joshua Beitler <joshbeitler@gmail.com>
2013-11-15 23:49:33 -0700

    Boss re-implimented

--- enemies/boss1.py
+++ enemies/boss1.py
@@ -26,6 +26,14 @@ class Boss():
         if self.alive == True:
             if self.ammo > 0:
                 self.ammo -= 1
+                if random.randint(1, 100) == 1:
+                    if not Globals.mute == True:
+                        self.lazer_2_snd.play()
+                else:
+                    if not Globals.mute == True:
+                        self.lazer_3_snd.play()
+                self.ammo -= 1
+                # self.lazer_1.play()
                 return BaddieBullet(self.x + (self.width / 2), (self.y + (self.height / 2)))
             else:
                 return None
@@ -36,6 +44,48 @@ class Boss():
         for bullet in self.bullets:
             if bullet.alive:
                 live_bullets.append(bullet)
+
+
+        # self.new_x = self.x + random.randint(-1,1)
+        self.new_y = self.y + self.speed
+        if self.new_x < back_wall:
+            self.setAlive(False)
+        # if self.new_y < upper_wall:
+            # self.new_y = upper_wall
+        # elif self.new_y + self.height > lower_wall:
+            # self.new_y = lower_wall - self.height
+        self.y = self.new_y
+
+        if random.randint(1, 255) == 1:
+            self.bullets.append(self.fire())
+
+        for bullet in self.bullets:
+            bullet.moveBullet()
+
+        self.bullets = live_bullets
+
+        spaceship_rect = pygame.Rect(Globals.spaceship.x, Globals.spaceship.y,Globals.spaceship.width,Globals.spaceship.height)
+
+        for bullet in self.bullets:
+            if bullet == None:
+                break
+            bullet_rect = pygame.Rect(bullet.x, bullet.y, bullet.width, bullet.height)
+
+            if bullet_rect.colliderect(spaceship_rect):
+                bullet.setAlive(False)
+                Globals.spaceship.health -= 1
+                self.lazer_1_snd.play()
+
+        self.y = self.new_y
+
+        if random.randint(1, 150) ==1:
+            self.bullets.append(self.fire())
+
+        self.bullets = live_bullets
+        if self.y >= 720:
+            self.setAlive(False)
+
+        return self.alive
 
 
         # self.new_x = self.x + random.randint(-1,1)
