c7159360d52b9061e14e251b906edaf6ba47f333
Kyler Tolleson <kylertoll@yahoo.com>
2013-11-16 00:07:47 -0700

    menu page

--- screens/MenuScreen.py
+++ screens/MenuScreen.py
@@ -18,8 +18,8 @@ class MenuScreen(GameScreen):
         self.text.drawTextLeft(surface, "Logan Cox", (255, 255, 255), 1025, 90)
         self.text.drawTextLeft(surface, "Arrow Keys or WASD to move!", (255, 255, 255), 425, 650)
         self.text.drawTextLeft(surface, "Blue powerups increase your speed!", (81, 212, 255), 350, 670)
-        self.text.drawTextLeft(surface, "Green powerups give you ammo!", (52, 255, 100), 420, 690)
-        self.text.drawTextLeft(surface, "Be careful when you get to difficulty 100!", (255, 255, 255), 300, 710)
+        self.text.drawTextLeft(surface, "Green powerups give you ammo!", (52, 255, 100), 405, 690)
+        self.text.drawTextLeft(surface, "Be careful when you get to difficulty 100!", (255, 255, 255), 308, 710)
 
     def update(self, *args):
         rect = pygame.Rect(0, 0, 1280, 720)
