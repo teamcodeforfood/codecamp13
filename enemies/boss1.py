ca7eb2698efc33fcbf8283b0b95e8e49f18df66b
Kyler Tolleson <kylertoll@yahoo.com>
2013-11-15 20:38:19 -0700

    changed boss image

--- enemies/boss1.py
+++ enemies/boss1.py
@@ -8,13 +8,13 @@ class Boss():
         self.width  = 100
         self.height = 100
         self.x      = 640
-        self.y      = 100
+        self.y      =10
         self.new_x  = x
         self.new_y  = y
         self.speed  = .2
         self.color  = (255, 0, 255)
         self.alive  = True
-        self.sprite_1 = pygame.image.load("resources/sprites/redcapsule.png")
+        self.sprite_1 = pygame.image.load("resources/sprites/roboticus2.png")
         self.boom_1 = pygame.mixer.Sound("resources/sound/boom_1.wav")
         self.bullets = []
         self.health = 10000
