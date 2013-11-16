870aaab850cce10c823126de8c5492a3ecf826c5
Joshua Beitler <joshbeitler@gmail.com>
2013-11-15 21:52:12 -0700

    Merged

--- screens/Hud.py
+++ screens/Hud.py
@@ -1,9 +1,10 @@
 import pygame
 from GameScreen import GameScreen
+from meter import Meter
 
 class Hud(GameScreen):
     def load(self):
-        pass
+        self.ammo_meter = Meter(100, 0, 100, 100, 100)
 
     def update(self, *args):
         self.score = args[0]
@@ -12,9 +13,14 @@ class Hud(GameScreen):
         self.missed = args[3]
         self.gamedifficulty = args[4]
 
+        self.ammo_meter.update(self.ammo)
+
     def draw(self, surface):
         self.text.drawTextLeft(surface, "Score    " + str(self.score), (255, 255, 255), 10, 35)
         self.text.drawTextLeft(surface, "Ammo    " + str(self.ammo), (255, 255, 255), 10, 55)
         self.text.drawTextLeft(surface, "Health   " + str(self.health), (255, 255, 255), 10, 75)
         self.text.drawTextLeft(surface, "Missed   " + str(self.missed), (255, 255, 255), 10, 95)
         self.text.drawTextLeft(surface, "Difficulty   " + str(self.gamedifficulty), (255, 255, 255), 585, 35)
+
+        self.ammo_meter.draw(surface)
+
