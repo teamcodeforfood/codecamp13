40e2f18ace5b4878f2f703bb199bfcf3e3891b54
Kyler Tolleson <kylertoll@yahoo.com>
2013-11-15 18:20:33 -0700

    made title page cooler

--- screens/MenuScreen.py
+++ screens/MenuScreen.py
@@ -9,8 +9,8 @@ class MenuScreen(GameScreen):
         rect = pygame.Rect(0, 0, 1280, 720)
         pygame.draw.rect(surface, (0, 0, 0), rect)
 
-        self.text.drawTextLeft(surface, "Galaxy Jam", (255, 255, 255), 50, 50)
-        self.text.drawTextLeft(surface, "press space to begin", (255, 255, 255), 50, 75)
+        self.text.drawTextLeft(surface, "Galaxy Jam", (51, 255, 255), 585, 340)
+        self.text.drawTextLeft(surface, "press space to begin", (255, 255, 255), 540, 380)
 
     def update(self, *args):
         if pygame.K_SPACE in args[0]:
