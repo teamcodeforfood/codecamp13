c49585fa24f3b13665aff980710f1fd73774f923
Kyler Tolleson <kylertoll@yahoo.com>
2013-11-15 23:50:31 -0700

    added names and team name to menu

--- screens/MenuScreen.py
+++ screens/MenuScreen.py
@@ -12,6 +12,10 @@ class MenuScreen(GameScreen):
         rect = pygame.Rect(0, 0, 1280, 720)
         self.text.drawTextLeft(surface, "Galaxy Jam", (51, 255, 255), 550, 340)
         self.text.drawTextLeft(surface, "press space to begin", (255, 255, 255), 475, 380)
+        self.text.drawTextLeft(surface, "Will Code for food", (255, 0, 0), 950, 30)
+        self.text.drawTextLeft(surface, "Josh Beitler", (255, 255, 255), 1000, 50)
+        self.text.drawTextLeft(surface, "Kyler Tolleson", (255, 255, 255), 975, 70)
+        self.text.drawTextLeft(surface, "Logan Cox", (255, 255, 255), 1025, 90)
 
     def update(self, *args):
         rect = pygame.Rect(0, 0, 1280, 720)
